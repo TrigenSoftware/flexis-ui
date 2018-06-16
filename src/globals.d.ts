53eb5a7e2563559349346c7e5bd23af82fb94a64
Dan Green <danon0404@gmail.com>
2018-06-16 17:28:22 +0700

    to ts wip

---
+++ src/globals.d.ts
@@ -0,0 +1,46 @@
+/**
+ * Global typings.
+ */
+
+type StateValue = boolean | number | string;
+
+interface StateMap {
+	[stateName: string]: StateValue;
+}
+
+interface AttributeMap {
+	className?: string
+	[attributeName: string]: StateValue | undefined
+}
+
+interface InheritedAttributes {
+	className?: string
+	[props: string]: any
+}
+
+type RuntimeStylesheet = {
+	(className: string, states: StateMap, inheritedAttributes: InheritedAttributes): AttributeMap
+	$root: string,
+	$namespace: string,
+	$depth: number,
+	$id: string | number,
+	$css?: string,
+
+	$get(localName: string): string | undefined;
+	$cssStates(stateMapping?: StateMap | null): StateMap;
+} & { [localName: string]: string }
+
+declare module '*.st.css' {
+	const stylesheet: RuntimeStylesheet;
+	export default stylesheet;
+}
+
+// declare module '*.svg' {
+// 	const urlToFile: string;
+// 	export default urlToFile;
+// }
+
+// declare module '*.css' {
+// 	const stylesheet: void;
+// 	export default stylesheet;
+// }
