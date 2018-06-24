f54223a8f5c6506af96ff8e6d4535645d85de368
Dan Green <danon0404@gmail.com>
2018-06-24 16:01:07 +0700

    build script mod

--- scripts/buildPackage.sh
+++ scripts/buildPackage.sh
@@ -4,7 +4,7 @@ rm -rf package
 mkdir package
 tsc --rootDir src --outDir package
 cp -R src/ package/
-del 'package/**/*.stories.js' 'package/**/*.stories.d.ts'
+del 'package/**/*.stories.js' 'package/**/*.stories.d.ts' 'package/**/*.stories.d.ts.map'
 del 'package/**/*.{ts,tsx}' '!package/**/*.d.ts'
 babel ./package -d ./package -s inline
 cp LICENSE package
