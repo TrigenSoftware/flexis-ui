ad19f9d321bb7ecc2ed7801e33f8f42013a99eea
Dan Green <danon0404@gmail.com>
2018-06-27 15:01:25 +0700

    globals.d.ts, deps upd

--- src/globals.d.ts
+++ src/globals.d.ts
@@ -34,12 +34,3 @@ declare module '*.st.css' {
 	const stylesheet: RuntimeStylesheet;
 	export default stylesheet;
 }
-
-// declare module '*.svg' {
-// 	const urlToFile: string;
-// 	export default urlToFile;
-// }
-// declare module '*.css' {
-// 	const stylesheet: void;
-// 	export default stylesheet;
-// }
