ad99de94cd3a0b49d0b9f6216b0ac122c3d912bd
dangreen <danon0404@gmail.com>
2018-10-02 18:48:01 +0700

    better bundle [ci skip]

--- scripts/buildPackage.sh
+++ scripts/buildPackage.sh
@@ -4,9 +4,9 @@ rm -rf package
 mkdir package
 tsc --rootDir src --outDir package
 cp -R src/ package/
-del 'package/**/*.stories.js' 'package/**/*.stories.d.ts' 'package/**/*.stories.d.ts.map'
-del 'package/**/*.{ts,tsx}' '!package/**/*.d.ts'
-babel ./package -d ./package -s inline
+NODE_ENV=production babel ./package -d ./package -s inline
+del 'package/**/*.stories.jsx' 'package/**/*.stories.d.ts' 'package/**/*.stories.d.ts.map'
+del 'package/**/*.{ts,tsx,jsx}' '!package/**/*.d.ts'
 cp LICENSE package
 cp package.json package
 cp README.md package
