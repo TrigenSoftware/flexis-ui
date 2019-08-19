cb63b17c2b0a3e4970b926792867dca326a798f8
dangreen <danon0404@gmail.com>
2019-08-19 12:34:37 +0700

    es.js

--- scripts/buildPackage.sh
+++ scripts/buildPackage.sh
@@ -6,8 +6,8 @@ rm -rf package
 cp -R src package
 tsc --rootDir src --outDir package
 del 'package/**/*.{ts,tsx}' '!package/**/*.d.ts'
-find package -name "*.js" -exec bash -c 'cp "$1" "${1%.js}".mjs' - '{}' \;
-find package -name "*.jsx" -exec bash -c 'cp "$1" "${1%.jsx}".mjs' - '{}' \;
+find package -name "*.js" -exec bash -c 'cp "$1" "${1%.js}".es.js' - '{}' \;
+find package -name "*.jsx" -exec bash -c 'cp "$1" "${1%.jsx}".es.js' - '{}' \;
 find package -name "*.jsx" -exec bash -c 'mv "$1" "${1%.jsx}".js' - '{}' \;
 NODE_ENV=production babel ./package -d ./package -s inline --keep-file-extension
 del 'package/**/*.jsx' 'package/stylable/**/*.mjs'
