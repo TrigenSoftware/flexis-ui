7034e671b18389dd042608783725c056fde7c266
dangreen <danon0404@gmail.com>
2019-08-19 11:00:45 +0700

    mjs and js

--- scripts/buildPackage.sh
+++ scripts/buildPackage.sh
@@ -6,8 +6,11 @@ rm -rf package
 cp -R src package
 tsc --rootDir src --outDir package
 del 'package/**/*.{ts,tsx}' '!package/**/*.d.ts'
-NODE_ENV=production babel ./package -d ./package -s inline
-del 'package/**/*.jsx'
+find package -name "*.js" -exec bash -c 'cp "$1" "${1%.js}".mjs' - '{}' \;
+find package -name "*.jsx" -exec bash -c 'cp "$1" "${1%.jsx}".mjs' - '{}' \;
+find package -name "*.jsx" -exec bash -c 'mv "$1" "${1%.jsx}".js' - '{}' \;
+NODE_ENV=production babel ./package -d ./package -s inline --keep-file-extension
+del 'package/**/*.jsx' 'package/stylable/**/*.mjs'
 cp LICENSE package
 cp package.json package
 cp README.md package
