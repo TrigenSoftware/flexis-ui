d21dbb5d9444c9db313edec3e896c7dd8566347f
dangreen <danon0404@gmail.com>
2019-08-25 20:10:23 +0700

    rm containers dir

--- containers/testStoryshots.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-apt-get update
-apt-get install -y rsync
-rsync -av /project /home/node --exclude node_modules --exclude .git --exclude .awcache
-cd /home/node/project
-yarn
-yarn build:storybook
-yarn jest test/storyshots
