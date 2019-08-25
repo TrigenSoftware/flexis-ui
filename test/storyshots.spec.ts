830106b2a7844f0b7e1ac49dc7e19061a24ec99f
dangreen <danon0404@gmail.com>
2019-08-25 18:59:59 +0700

    storyshots mod

--- test/storyshots.spec.ts
+++ test/storyshots.spec.ts
@@ -1,9 +1,33 @@
-import initStoryshots from '@trigen/scripts-plugin-storybook/jest/storyshots';
+import initStoryshots, {
+	getMatchOptions
+} from '@trigen/scripts-plugin-storybook/jest/storyshots';
 
 process.env.FLEXISUI_STORIES = JSON.stringify(true);
 
-if (process.platform === 'linux') {
-	initStoryshots();
-} else {
-	it.skip('Storyshots', () => {});
-}
+initStoryshots({
+	getMatchOptions(info) {
+
+		const options = getMatchOptions(info);
+		let failureThreshold = 0.2;
+
+		switch (options.customSnapshotIdentifier) {
+
+			case 'ScrollArea__with-table':
+				failureThreshold = 5;
+				break;
+
+			case 'Table__with-rows':
+			case 'Table__with-order':
+				failureThreshold = 1;
+				break;
+
+			default:
+		}
+
+		return {
+			failureThresholdType: 'percent',
+			failureThreshold,
+			...options
+		};
+	}
+});
