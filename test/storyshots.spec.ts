4a1e9aa1a566b10dd1e14add79c9591ce69f1bca
dangreen <danon0404@gmail.com>
2019-09-06 22:09:38 +0300

    meh99

--- test/storyshots.spec.ts
+++ test/storyshots.spec.ts
@@ -8,7 +8,7 @@ initStoryshots({
 	getMatchOptions(info) {
 
 		const options = getMatchOptions(info);
-		const failureThreshold = 0.05;
+		const failureThreshold = 0.1;
 
 		return {
 			failureThresholdType: 'percent',
